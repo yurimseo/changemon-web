770c0524a4a56153eb3c4e3b6857d2ec94a93cc1
yurimseo <56903214+yurimseo@users.noreply.github.com>
2026-07-19 03:31:28 +0900

    Update EggCard.tsx

--- components/EggCard.tsx
+++ components/EggCard.tsx
@@ -3,13 +3,30 @@ import Image from "next/image";
 export default function EggCard() {
   return (
     <div className="flex justify-center py-12">
-      <Image
-        src="public/eggs/default_egg.png"
-        alt="창제알"
-        width={180}
-        height={180}
-        priority
-      />
+      <div className="relative w-48 h-48">
+
+        {/* 풀 */}
+        <Image
+          src="/eggs/grass.png"
+          alt="풀"
+          width={180}
+          height={60}
+          className="absolute bottom-0 left-1/2 -translate-x-1/2"
+          priority
+        />
+
+        {/* 알 */}
+        <div className="absolute bottom-6 left-1/2 -translate-x-1/2 animate-bounce">
+          <Image
+            src="/eggs/egg.png"
+            alt="창제알"
+            width={120}
+            height={120}
+            priority
+          />
+        </div>
+
+      </div>
     </div>
   );
 }
